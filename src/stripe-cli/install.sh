c9e23068e5f235f20b5013e5b3220ec60c1900dd
Thanan Traiongthawon <95660+nullcoder@users.noreply.github.com>
2023-06-01 09:47:38 +0000

    Remove `sudo` usages.

--- src/stripe-cli/install.sh
+++ src/stripe-cli/install.sh
@@ -82,8 +82,8 @@ fi
 echo "Downloading Stripe CLI..."
 
 . /etc/os-release
-curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | sudo tee /usr/share/keyrings/stripe.gpg
-echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | sudo tee -a /etc/apt/sources.list.d/stripe.list
+curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | tee /usr/share/keyrings/stripe.gpg
+echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | tee -a /etc/apt/sources.list.d/stripe.list
 apt-get update
 apt-get -y install "stripe${version_suffix}"
 echo "Done!"
